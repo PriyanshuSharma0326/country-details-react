fd246c630f54c1cf8255ba96be347c934efb5913
PriyanshuSharma0326 <ps940016@gmail.com>
2023-08-13 13:56:00 +0530

    Initialize project

---
+++ src/components/InputForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/InputForm.css';
+
+function InputForm() {return (
+        <div className='form-container'>
+            <form action="" className="country-selection-form">
+            </form>
+        </div>
+    );
+}
+
+export default InputForm;
